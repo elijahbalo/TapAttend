5725bd111f9b402d383e9749543f3e5c5a55f329
elijahbalo <jah.balo16@gmail.com>
2017-10-05 12:49:26 -0400

    setup

--- tapAttend/ionic/mobile/src/app/app.component.ts
+++ tapAttend/ionic/mobile/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Platform, MenuController, Nav } from 'ionic-angular';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ListPage } from '../pages/list/list';
 
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
--- tapAttend/ionic/mobile/src/app/app.module.ts
+++ tapAttend/ionic/mobile/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
 
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -16,6 +17,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HelloIonicPage,
     ItemDetailsPage,
     ListPage
+
   ],
   imports: [
     BrowserModule,
@@ -27,6 +29,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HelloIonicPage,
     ItemDetailsPage,
     ListPage
+    
   ],
   providers: [
     StatusBar,
