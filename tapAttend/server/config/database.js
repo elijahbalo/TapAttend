a08ae063bde1500f3b7e4294b2c501c66b06b867
elijahbalo <jah.balo16@gmail.com>
2017-10-12 22:44:39 -0400

    server setup

---
+++ tapAttend/server/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://elijah_balo:eli321,balo@ds119565.mlab.com:19565/tapattend' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
