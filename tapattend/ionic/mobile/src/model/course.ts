04d11ef69b8c1c5d7145d670b1de007e9209696e
Jamar-Mitchell <jamar-mitchell@hotmail.com>
2017-11-09 12:38:41 -0500

    added classes

---
+++ tapattend/ionic/mobile/src/model/course.ts
@@ -0,0 +1,13 @@
+export class Course{
+    
+   constructor(
+   public courseCode: String,
+   public name: String,
+   public id: String,
+   public profName: String
+   ){}
+
+
+
+
+}
---
+++ tapattend/ionic/mobile/src/model/user.ts
@@ -0,0 +1,16 @@
+export class User{
+     
+    constructor(
+    public userName: String,
+    public firstName: String,
+    public lastName: String,
+    public studentId: String,
+    public password: String,    
+    public token: String    
+
+    ){}
+
+
+
+
+}
