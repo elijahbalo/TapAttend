86553ee00e9b1f2929aabbb4ff6f5dad3f784059
elijahbalo <jah.balo16@gmail.com>
2017-11-16 11:52:05 -0500

    fixing project path

---
+++ tapattend/ionic/mobile/src/services/auth.service.ts
@@ -0,0 +1,40 @@
+import { UrlProvider } from "../util/url-provider";
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+//Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+
+@Injectable()
+export class AuthService {
+  // Resolve HTTP using the constructor
+  constructor(private http: Http) { }
+  // private instance variable to hold base url
+  private url = UrlProvider.url + '/api/oauth/token';
+
+
+  signIn(username, password) {
+    let data = {
+      "grant_type": "password",
+      "client_id": "android",
+      "client_secret": "myClientSecret",
+      "username": username,
+      "password": password
+    }
+
+    let body = JSON.stringify(data);
+    let head = new Headers({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(this.url, body, { headers: head })
+      // ...and calling .json() on the response to return data
+      .map((res) => res.json())
+      //..errors if any
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'))
+  }
+
+
+}
