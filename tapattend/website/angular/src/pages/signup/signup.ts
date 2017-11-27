ea3c82580205a7e1514985df030e956dbc3f7527
tobitisu@yahoo.co.uk <tobitisu@yahoo.co.uk>
2017-11-27 17:21:09 -0500

    Created Sign Up Form

--- tapattend/website/angular/src/pages/signup/signup.ts
+++ tapattend/website/angular/src/pages/signup/signup.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
    selector: 'page-signup',
-  templateUrl: 'signup.html'
+  templateUrl: 'signup.html',
+  styleUrls: ['./signUp.css']
 })
 export class SignUpPage {
  
